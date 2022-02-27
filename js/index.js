b3d9351697ef6fbcf459a1d4d22499690bb04be8
jicu mollick <jikumollick82@gmail.com>
2022-02-27 23:15:09 +0600

    card added

--- js/index.js
+++ js/index.js
@@ -19,15 +19,30 @@ const displayDog = (dogs) => {
         
     const div = document.createElement('div');
     div.classList.add('col-md-4');
-        div.innerHTML = `
-        <h2> ${dog.name} </h2>
-        <p>${dog.temperament}</p>
-        <h4>${dog.weight.imperial}</h4>
-        <img src='${dog.image.url}' class= 'img-fluid'>
+        div.innerHTML = `     
+  <div class="card">
+    <div class="image">
+      <img src="${dog.image.url}" class='img-fluid' />
+    </div>
+    <div class="card-inner">
+      <div class="header">
+        <h3>Name: ${dog.name}</h3>
+        <h4>Weight: ${dog.weight.imperial}</h4>
+    </div>
+    <div class="content">
+      <p>Type: ${dog.temperament}</p>
+    </div>
+      </div>
+  </div>
+
+     
         `;
         main.appendChild(div);
 
     }
-
-
 }
+
+{/* <h2> ${dog.name} </h2>
+<p>${dog.temperament}</p>
+<h4>${dog.weight.imperial}</h4>
+<img src='${dog.image.url}' class= 'img-fluid'> */}
